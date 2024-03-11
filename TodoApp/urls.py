a39f18ab30891f4bdcae1009d0e2b5dd75a5475c
Tanjil Ahmed Ovi <ovi@usernoMacBook-Air.local>
2024-03-11 23:08:52 +0900

    apiv1

--- TodoApp/urls.py
+++ TodoApp/urls.py
@@ -1,8 +1,19 @@
 from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
+from rest_framework import routers
+
+from todoApi import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     path('apiV1/todo/', include('todoApi.urls')),
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
+
+urlpatterns += router.urls
