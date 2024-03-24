5e65f75d7a697a1501f44fe094360fc600c83c52
Tanjil Ahmed Ovi <tanzil.ovi578@gmail.com>
2024-03-24 20:14:17 +0900

    code refactoring

--- todoApi/urls.py
+++ todoApi/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('add/', api_views.todoAdd, name='add'),
     path('<int:pk>/', api_views.todoSingleDetails, name='details'),
     path('edit/<int:pk>/', api_views.todoEdit, name='edit'),
-    path('remove/<int:pk>/', api_views.todoDelete),
+    path('remove/<int:pk>/', api_views.todoDelete, name='remove'),
 ]
