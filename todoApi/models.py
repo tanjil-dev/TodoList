25e9ecd2dce4fa856839527d4efeaab92d5903d6
Tanjil Ahmed Ovi <tanzil.ovi578@gmail.com>
2024-03-24 16:03:17 +0900

    crud apis

--- todoApi/models.py
+++ todoApi/models.py
@@ -4,10 +4,10 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Todo(models.Model):
-    title = models.CharField(max_length=30)
+    title = models.CharField(max_length=30, blank=True)
     is_done = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True, blank=True)
-    user_name = models.OneToOneField(User, on_delete=models.DO_NOTHING)
+    user_name = models.OneToOneField(User, on_delete=models.DO_NOTHING, blank=True)
 
     def __str__(self):
         return self.title
--- todoApi/urls.py
+++ todoApi/urls.py
@@ -3,5 +3,8 @@ from . import api_views
 
 urlpatterns = [
     path('', api_views.todoList, name='list'),
-    # path('todo-add/', views.todoList)
+    path('add/', api_views.todoAdd, name='add'),
+    path('<int:pk>/', api_views.todoSingleDetails, name='details'),
+    path('edit/<int:pk>/', api_views.todoEdit, name='edit'),
+    path('remove/<int:pk>/', api_views.todoDelete),
 ]
