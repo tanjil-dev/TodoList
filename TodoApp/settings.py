72d66f1c3f3ac717fdb953b42debbe8f7659fb48
Tanjil Ahmed Ovi <tanzil.ovi578@gmail.com>
2024-06-12 18:57:41 +0600

    static root for deploy

--- TodoApp/settings.py
+++ TodoApp/settings.py
@@ -103,7 +103,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.environ.get('STATIC_ROOT')
+STATIC_ROOT = '/home/tanjil95/TodoList/user/static'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
