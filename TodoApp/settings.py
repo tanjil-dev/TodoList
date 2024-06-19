27693c41d3b64325cbdca43a83e814b27cdee63d
Tanjil Ahmed Ovi <tanzil.ovi578@gmail.com>
2024-06-20 05:45:52 +0600

    mysqldb and auto logout

--- TodoApp/settings.py
+++ TodoApp/settings.py
@@ -8,9 +8,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-
-# Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -55,19 +52,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'TodoApp.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'todo',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'tanjil123'
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -98,10 +92,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.environ.get('STATIC_ROOT')
 
@@ -109,3 +99,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+SESSION_COOKIE_AGE = 300
+SESSION_SAVE_EVERY_REQUEST = True
