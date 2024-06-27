0f25037d46af34dfefa22b0d744bcea78ce96ea2
Tanjil Ahmed Ovi <tanzil.ovi578@gmail.com>
2024-06-28 04:51:16 +0600

    gunicorn prod file

--- config/prod.py
+++ config/prod.py
@@ -9,11 +9,11 @@ workers = multiprocessing.cpu_count() * 2 + 1
 # The socket to bind
 bind = "0.0.0.0:8000"
 # Write access and error info to /var/log
-accesslog = "/var/log/gunicorn/access.log"
-errorlog = "/var/log/gunicorn/error.log"
+accesslog = "/Users/ovi/var/run/gunicorn/dev.pid"
+errorlog = "/Users/ovi/var/run/gunicorn/error.log"
 # Redirect stdout/stderr to log file
 capture_output = True
 # PID file so you can easily fetch process ID
-pidfile = "/var/run/gunicorn/prod.pid"
+pidfile = "/Users/ovi/var/run/gunicorn/dev.pid"
 # Daemonize the Gunicorn process (detach & enter background)
 daemon = True
