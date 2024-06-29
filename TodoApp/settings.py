642d2a785a71dfb6965a2d8cffb85066f09e18ce
Tanjil Ahmed Ovi <tanzil.ovi578@gmail.com>
2024-06-29 19:45:22 +0600

    debug variable

--- TodoApp/settings.py
+++ TodoApp/settings.py
@@ -4,7 +4,7 @@ load_dotenv()
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = False
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
