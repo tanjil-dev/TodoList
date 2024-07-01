fd182b1a6aba9a7173a60dd3543c765ed8c9979b
Tanjil Ahmed Ovi <tanzil.ovi578@gmail.com>
2024-07-01 12:51:05 +0600

    some changes in url

--- todoApi/urls.py
+++ todoApi/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from . import api_views as view
+from todoApi.api_views import *
 
 urlpatterns = [
-    path('list/', view.todoList, name='list'),
-    path('add/', view.todoAdd, name='add'),
-    path('<int:pk>/', view.todoSingleDetails, name='details'),
-    path('edit/<int:pk>/', view.todoEdit, name='edit'),
-    path('remove/<int:pk>/', view.todoDelete, name='remove'),
+    path('list/', todoList, name='list'),
+    path('add/', todoAdd, name='add'),
+    path('<int:pk>/', todoSingleDetails, name='details'),
+    path('edit/<int:pk>/', todoEdit, name='edit'),
+    path('remove/<int:pk>/', todoDelete, name='remove'),
 ]
